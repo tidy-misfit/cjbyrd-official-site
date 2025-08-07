868dafe191ea6c86444d0ee36afc2f75bc3875b6
User1 <214354515+tidy-misfit@users.noreply.github.com>
2025-08-07 15:24:02 +0900

    Edited featured books
    Reduced max slides per view, removed books not on sale

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,7 +82,7 @@ let swiperFeatured = new Swiper('.featured__swiper', {
 
   breakpoints: {
     1150: {
-        slidesPerView: 4,
+        slidesPerView: 3,
         centeredSlides: false,
     }
   }
